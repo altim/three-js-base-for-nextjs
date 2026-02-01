b96b7953de7a8819612e98a23d471a5235197bac
Aleksandar Timic <aleksandartimic@gmail.com>
2026-02-01 20:50:56 +0100

    Revert "Initial commit"
    This reverts commit 2a1d5f217be2cb010f783cd6344c802efedb573d.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,7 @@
 "use client";
 
-import Navigation from "../components/navigation/Navigation";
-import Scene from "../components/Scene/Scene";
+import SmokeScene from "../components/Scene/Scene";
 
 export default function Home() {
-  return (
-    <>
-      <Navigation />
-      <Scene />
-    </>
-  );
+  return <SmokeScene />;
 }
--- app/components/Scene/components/Experience/Experience.tsx
+++ app/components/Scene/components/Experience/Experience.tsx
@@ -1,40 +1,27 @@
 "use client";
 
-import DJController from "@/app/components/DJController/DJController";
-import { PerspectiveCamera } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import * as THREE from "three";
-import fragmentShader from "../../shaders/fragment.glsl";
 import vertexShader from "../../shaders/vertex.glsl";
+import fragmentShader from "../../shaders/fragment.glsl";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 
 export default function SmokeExperience() {
   const shaderMaterialRef = useRef<THREE.ShaderMaterial>(null);
-  const cameraRef = useRef<THREE.PerspectiveCamera>(null);
-  const objectRef = useRef<THREE.Group>(null);
-
-  useEffect(() => {
-    cameraRef.current?.lookAt(0.2, 0.2, 0.2);
-  }, []);
 
   // Update shader uniforms each frame
-  useFrame((state, delta) => {
+  useFrame((state) => {
     if (!shaderMaterialRef.current) return;
 
     // Update time uniform for constant animation
     shaderMaterialRef.current.uniforms.uTime.value = state.clock.elapsedTime;
-
-    if (objectRef.current) {
-      objectRef.current.rotation.y += delta * 0.5;
-    }
   });
 
   return (
     <>
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[3, 3, 3]} intensity={2} />
-      <mesh position={[0, 0, 0]} rotation={[-Math.PI * 0.5, 0, 0]}>
-        <planeGeometry args={[3, 3, 64, 64]} />
+      <mesh position={[0, 0, 0]}>
+        <planeGeometry args={[1, 1, 32, 32]} />
         <shaderMaterial
           ref={shaderMaterialRef}
           vertexShader={vertexShader}
@@ -42,18 +29,14 @@ export default function SmokeExperience() {
           uniforms={{
             uTime: { value: 0 },
           }}
-          wireframe={true}
+          transparent={true}
+          depthWrite={false}
+          blending={THREE.NormalBlending}
         />
       </mesh>
-      <group position={[0.2, 0, 0.2]} ref={objectRef}>
-        <DJController />
-      </group>
 
-      <PerspectiveCamera
-        ref={cameraRef}
-        position={[0.7, 0.9, 0.6]}
-        makeDefault
-      />
+      <PerspectiveCamera position={[0, 0, 2]} makeDefault />
+      <OrbitControls />
     </>
   );
 }
